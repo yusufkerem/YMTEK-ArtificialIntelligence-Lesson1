c6d25f08947359d5cc0bb6c9f92eb199fec295aa
Yusuf Kerem <47509437+yusufkerem@users.noreply.github.com>
2019-12-04 19:02:03 +0300

    Add files via upload

---
+++ ODEV/odev1.py
@@ -0,0 +1,4 @@
+#ODEV 1
+#Ödev için artık gerçek hayat verileriyle uğraşacağız :)
+#şimdi githubdaki ödev reposunda bulunan "kc_house_data.csv" dosyasından sadece sqft_living15 ve price özelliğini kullanarak
+#evin metrekaresine göre fiyat tahmini yapacağız. Bunu yapan lineer regresyon modelini oluşturun ve veriyi dağılım grafiğinde görselleştirin.
